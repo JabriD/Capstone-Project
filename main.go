17c6fc872a109222092840944eabb34024a2c721
JabriD <105766106+JabriD@users.noreply.github.com>
2022-12-06 00:05:51 +0000

    added standings grab, structs designed

--- main.go
+++ main.go
@@ -9,44 +9,8 @@ import (
 	"github.com/davecgh/go-spew/spew"
 )
 
-type AllPlayers struct {
-	League struct {
-		Standard []struct {
-			FirstName            string `json:"firstName"`
-			LastName             string `json:"lastName"`
-			TemporaryDisplayName string `json:"temporaryDisplayName,omitempty"`
-			PersonID             string `json:"personId"`
-			TeamID               string `json:"teamId"`
-			Jersey               string `json:"jersey"`
-			IsActive             bool   `json:"isActive"`
-			Pos                  string `json:"pos"`
-			HeightFeet           string `json:"heightFeet"`
-			HeightInches         string `json:"heightInches"`
-			WeightPounds         string `json:"weightPounds"`
-			DateOfBirthUTC       string `json:"dateOfBirthUTC"`
-			Teams []struct {
-				TeamID      string `json:"teamId"`
-				SeasonStart string `json:"seasonStart"`
-				SeasonEnd   string `json:"seasonEnd"`
-			} `json:"teams"`
-			Draft struct {
-				TeamID     string `json:"teamId"`
-				PickNum    string `json:"pickNum"`
-				RoundNum   string `json:"roundNum"`
-				SeasonYear string `json:"seasonYear"`
-			} `json:"draft"`
-			NbaDebutYear    string `json:"nbaDebutYear"`
-			YearsPro        string `json:"yearsPro"`
-			CollegeName     string `json:"collegeName"`
-			LastAffiliation string `json:"lastAffiliation"`
-			Country         string `json:"country"`
-			IsallStar       bool   `json:"isallStar,omitempty"`
-		} `json:"standard"`
-	} `json:"league"`
-}
-
-func main() {
-	res, err := http.Get("http://data.nba.net/10s/prod/v1/2018/players.json")
+func getPlayers() {
+	res, err := http.Get("http://data.nba.net/10s/prod/v1/2022/players.json")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -62,6 +26,30 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	spew.Dump(players)
+}
+
+func getTeams () {
+	res, err := http.Get("http://data.nba.net/prod/v1/current/standings_conference.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	bs, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var allTeams Standings
+	err = json.Unmarshal(bs, &allTeams)
+	if err != nil {
+		log.Fatal(err)
+	}
+	spew.Dump(allTeams)
+}
+
+func main() {
+	//getPlayers()
+	getTeams()
 }
---
+++ types.go
@@ -0,0 +1,69 @@
+package main
+
+type AllPlayers struct {
+	League struct {
+		Standard []struct {
+			FirstName            string `json:"firstName"`
+			LastName             string `json:"lastName"`
+			TemporaryDisplayName string `json:"temporaryDisplayName,omitempty"`
+			PersonID             string `json:"personId"`
+			TeamID               string `json:"teamId"`
+			Jersey               string `json:"jersey"`
+			IsActive             bool   `json:"isActive"`
+			Pos                  string `json:"pos"`
+			HeightFeet           string `json:"heightFeet"`
+			HeightInches         string `json:"heightInches"`
+			WeightPounds         string `json:"weightPounds"`
+			DateOfBirthUTC       string `json:"dateOfBirthUTC"`
+			Teams []struct {
+				TeamID      string `json:"teamId"`
+				SeasonStart string `json:"seasonStart"`
+				SeasonEnd   string `json:"seasonEnd"`
+			} `json:"teams"`
+			Draft struct {
+				TeamID     string `json:"teamId"`
+				PickNum    string `json:"pickNum"`
+				RoundNum   string `json:"roundNum"`
+				SeasonYear string `json:"seasonYear"`
+			} `json:"draft"`
+			NbaDebutYear    string `json:"nbaDebutYear"`
+			YearsPro        string `json:"yearsPro"`
+			CollegeName     string `json:"collegeName"`
+			LastAffiliation string `json:"lastAffiliation"`
+			Country         string `json:"country"`
+			IsallStar       bool   `json:"isallStar,omitempty"`
+		} `json:"standard"`
+	} `json:"league"`
+}
+
+
+type Standings struct {
+	League struct {
+		Standard struct {
+			SeasonYear    int `json:"seasonYear"`
+			SeasonStageID int `json:"seasonStageId"`
+			Conference    struct {
+				East []struct {
+					TeamID                 string `json:"teamId"`
+					TeamSitesOnly          struct {
+						TeamKey            string `json:"teamKey"`
+						TeamName           string `json:"teamName"`
+						TeamCode           string `json:"teamCode"`
+						TeamNickname       string `json:"teamNickname"`
+						TeamTricode        string `json:"teamTricode"`
+					} `json:"teamSitesOnly"`
+				} `json:"east"`
+				West []struct {
+					TeamID                 string `json:"teamId"`
+					TeamSitesOnly          struct {
+						TeamKey            string `json:"teamKey"`
+						TeamName           string `json:"teamName"`
+						TeamCode           string `json:"teamCode"`
+						TeamNickname       string `json:"teamNickname"`
+						TeamTricode        string `json:"teamTricode"`
+					} `json:"teamSitesOnly"`
+				} `json:"west"`
+			} `json:"conference"`
+		} `json:"standard"`
+	} `json:"league"`
+}
